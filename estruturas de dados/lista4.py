1e50f39636874a654ba647fb38e8bce56293eb58
gabriel9966 <gs06753@gmail.com>
2023-03-09 16:44:03 -0300

    1

---
+++ estruturas de dados/lista4.py
@@ -0,0 +1,10 @@
+lista = [0,3,4,1,6,42,26,2,96,32,23]
+
+del lista[3]#deleta 
+print(lista)
+
+d = 10 in lista
+print(d)
+
+lista.append(54)
+print(lista)
---
+++ função/built_in.py
@@ -0,0 +1,21 @@
+s = "oi tudo bem com vc, td "
+a = s.upper()
+print(a)
+b = a.split()#quebra por espaços
+print(b)
+c = s.split("t")# quebra quando acha "t"
+print(c)
+
+#Função String
+
+d = s.capitalize()
+print(d)#deixa a primeira letra maiscula
+
+e = s.count("t")#conta quantas letras # tem
+print(e)
+f = s.isalnum()# é toda de numeros
+print(f)
+# endSwith = termina com a letra ()
+
+g = "78172"
+print(g.isalnum())
