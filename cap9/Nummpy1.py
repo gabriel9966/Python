de5633b0f59bf5993c787e6b900b695b57dcb631
gabriel9966 <gs06753@gmail.com>
2023-05-05 15:38:45 -0300

    cap9

--- cap9/Nummpy1.py
+++ cap9/Nummpy1.py
@@ -1,3 +1,7 @@
-import numpy
-arr1 = numpy.array([1,2,3,4,5,6,7,8,9,10])
+#anaconda 3.10
+import numpy as np # pacote para computação numerica
+arr1 = np.array([1,2,3,4,5,6,7,8,9,10])
 print(arr1)
+print(type(arr1))
+print(arr1.shape)#verifica o formato do array
+
