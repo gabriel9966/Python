6bd02eaca5e6cf7c156e4b4db1befac6205a6640
gabriel9966 <gs06753@gmail.com>
2023-05-05 16:38:26 -0300

    cap9

--- cap9/Nummpy1.py
+++ cap9/Nummpy1.py
@@ -4,4 +4,14 @@ arr1 = np.array([1,2,3,4,5,6,7,8,9,10])
 print(arr1)
 print(type(arr1))
 print(arr1.shape)#verifica o formato do array
+#array numpy são mais eficientes
+print(arr1[2:5])#[inclusivo:exclusivo]
+indices = [1,2,3,4,5]
+print(arr1[indices])
+par = (arr1 % 2 == 0)#uma mascara booleana para elementos pares, so seve para array numpy
+print(arr1[par])
 
+try:
+    arr1[0] = "oi"
+except:
+    print("So pode ser usado numeros no array numpy")
