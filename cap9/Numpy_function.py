f6bf7aa7c89f237f96f04c50df3a9480aaf581a1
gabriel9966 <gs06753@gmail.com>
2023-05-10 16:44:07 -0300

    Update Numpy_function.py

--- cap9/Numpy_function.py
+++ cap9/Numpy_function.py
@@ -11,3 +11,14 @@ print(arr2)
 print("tipo do array2:",type(arr2))
 print("tipo do array2:",arr2.shape)
 print(arr2.dtype)#tipo de dado nesse array
+
+arr3 = np.zeros(10)
+print(arr3)
+arr3[5] = 6
+print(arr3)
+
+arr4 = np.eye(4)#retorna 1 em diagonal e 0 no resto
+print("\n",arr4)#matriz diagonal
+
+arr5 = np.diag(np.array([1,2,3,4]))#matriz diagonal
+print("\n",arr5)
