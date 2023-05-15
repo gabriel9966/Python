29dc4fa155cfbb6bc64e88a7a8b12b54fb9e8163
gabriel9966 <gs06753@gmail.com>
2023-05-15 16:59:51 -0300

    cap9

--- cap9/Matematica_array.py
+++ cap9/Matematica_array.py
@@ -11,3 +11,27 @@ print("Produto:",produto)
 
 soma_acumulada = np.cumsum(arr1)
 print("Soma acumulada:",soma_acumulada)
+
+arr2 = [1,2,3]
+arr3 = [4,5,6]
+
+
+soma_arrays = np.add(arr2,arr3)
+print("Soma dos arrasy:",soma_arrays)
+
+arr4 = np.array([[1,2],[4,5]])
+arr5 = np.array([[4,5],[1,2]])
+print(arr4.shape)
+print(arr5.shape)
+
+mult = np.dot(arr4,arr5)
+print("mult:",mult)
+
+#comparação de array 
+arr5 = [1,2,3,4]
+arr6 = [4,3,2,1]
+ 
+print(arr5 == arr6)
+ 
+comparacao_global = np.array_equal(arr5,arr6)
+print(comparacao_global)
