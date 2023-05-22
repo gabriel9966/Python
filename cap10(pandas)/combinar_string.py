5188f9a161cbe08e7a1b0b8b115bd1be99e23512
gabriel9966 <gs06753@gmail.com>
2023-05-22 17:57:38 -0300

    cap10

--- cap10(pandas)/combinar_string.py
+++ cap10(pandas)/combinar_string.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 df  = pd.read_csv("cap10(pandas)\dataset.csv")
+
+df["Pedido_Segmento"] = df["ID_Pedido"].str.cat(df["Segmento"], sep="-")#str para transformar em string
+
+print(df.head())
