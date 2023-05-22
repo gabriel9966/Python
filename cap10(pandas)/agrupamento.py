0b7a06c66615c2e0450b9a859bbd2b0727043ca1
gabriel9966 <gs06753@gmail.com>
2023-05-22 15:22:01 -0300

    cap10

--- cap10(pandas)/agrupamento.py
+++ cap10(pandas)/agrupamento.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv("cap10(pandas)\dataset.csv")
+df = pd.read_csv("cap10(pandas)\dataset.csv")#le o csv
+
 
 a = df[["Segmento","Regiao","Valor_Venda"]].groupby(["Segmento","Regiao"]).mean()#função de agrupamento grounpby, mean = média
 print(a)
